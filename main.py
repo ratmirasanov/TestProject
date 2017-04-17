a066168f83bdd4e8c1df107b451b65a4d3d51275
Ратмир <ratmir@MacBook-Pro-Ratmir.local>
2017-04-17 19:56:57 +0300

    CSV-file example with calculations

--- main.py
+++ main.py
@@ -1,2 +1,47 @@
-if __name__ == '__main__':
-    print('hello csv')
+#Reading CSV-file to list of lists.
+import csv
+exampleFile = open('Sacramentorealestatetransactions.csv')
+exampleReader = csv.reader(exampleFile)
+exampleData = list(exampleReader)
+
+
+#Declaration of variables.
+i = 0
+listWithPrices = []
+listWithSquareFootage = []
+
+
+#Creation of two lists.
+for row in exampleData:
+        listWithPrices.append(exampleData[i][9])
+        listWithSquareFootage.append(exampleData[i][6])
+        i += 1
+
+
+#Remove the first element of the each list.
+listWithPrices.remove('price')
+listWithSquareFootage.remove('sq__ft')
+
+
+#Converting string-list to int-list.
+for index, elem in enumerate(listWithPrices):
+                listWithPrices[index] = int(elem)
+
+for index, elem in enumerate(listWithSquareFootage):
+                listWithSquareFootage[index] = int(elem)
+
+
+#Calculations.
+maxPriceOfRealEstate = max(listWithPrices)
+minPriceOfRealEstate = min(listWithPrices)
+averageSquareFootage = sum(listWithSquareFootage) / len(listWithSquareFootage)
+averagePrice = sum(listWithPrices) / len(listWithPrices)
+
+
+#Printing results.
+print('Max price of real estate -- ' + str(exampleData[listWithPrices.index(maxPriceOfRealEstate)+1])
+      + ' is: ' + str(maxPriceOfRealEstate) + '$.')
+print('Min price of real estate -- ' + str(exampleData[listWithPrices.index(minPriceOfRealEstate)+1])
+      + ' is: ' + str(minPriceOfRealEstate) + '$.')
+print('Average square footage is -- ' + str(averageSquareFootage) + ' square foots.')
+print('Average price is -- ' + str(averagePrice) + '$.')
