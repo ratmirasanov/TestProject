5d60b609dcd0132a52838c390298f6cffc1781f8
ratmirasanov <asf1eh14i7d5fd3s2f3f>
2017-07-12 13:04:02 +0300

    1. Fixed all bugs. 2. Refactor code.

--- main.py
+++ main.py
@@ -1,47 +1,45 @@
 #Reading CSV-file to list of lists.
 import csv
-exampleFile = open('Sacramentorealestatetransactions.csv', 'rU')
+exampleFile = open('Badprice.csv', 'rU')
 exampleReader = csv.reader(exampleFile)
 exampleData = list(exampleReader)
 
 
 #Declaration of variables.
-i = 0
-listWithPrices = []
-listWithSquareFootage = []
+maxValue = 0
+minValue = 0
+maxIndex = ''
+minIndex = ''
+avgPrice = 0
+avgSquareFootage = 0
+countPrice = 0
+countSquareFootage = 0
 
 
 #Creation of two lists.
 for row in exampleData:
-        listWithPrices.append(exampleData[i][9])
-        listWithSquareFootage.append(exampleData[i][6])
-        i += 1
-
-
-#Remove the first element of the each list.
-listWithPrices.remove('price')
-listWithSquareFootage.remove('sq__ft')
-
-
-#Converting string-list to int-list.
-for index, elem in enumerate(listWithPrices):
-                listWithPrices[index] = int(elem)
-
-for index, elem in enumerate(listWithSquareFootage):
-                listWithSquareFootage[index] = int(elem)
-
-
-#Calculations.
-maxPriceOfRealEstate = max(listWithPrices)
-minPriceOfRealEstate = min(listWithPrices)
-averageSquareFootage = sum(listWithSquareFootage) / len(listWithSquareFootage)
-averagePrice = sum(listWithPrices) / len(listWithPrices)
+    if row[9].isalpha():
+        continue
+    else:
+        if float(row[9]) > 0 and float(row[6]) > 0:
+            avgPrice += float(row[9])
+            avgSquareFootage += float(row[6])
+            countPrice += 1
+            countSquareFootage += 1
+            if (maxValue < float(row[9])):
+                 maxValue = float(row[9])
+                 maxIndex = row[0]
+            if (minValue > float(row[9])):
+                 minValue = float(row[9])
+                 minIndex = row[0]
 
 
 #Printing results.
-print('Max price of real estate -- ' + str(exampleData[listWithPrices.index(maxPriceOfRealEstate)+1])
-      + ' is: ' + str(maxPriceOfRealEstate) + '$.')
-print('Min price of real estate -- ' + str(exampleData[listWithPrices.index(minPriceOfRealEstate)+1])
-      + ' is: ' + str(minPriceOfRealEstate) + '$.')
-print('Average square footage is -- ' + str(averageSquareFootage) + ' square foots.')
-print('Average price is -- ' + str(averagePrice) + '$.')
+for row in exampleData:
+    if (row[0] == maxIndex):
+        print('Max price of real estate -- ' + str(row) + ' is: ' + str(maxValue) + '$.')
+    if (row[0] == minValue):
+        print('Min price of real estate -- ' + str(row) + ' is: ' + str(minValue) + '$.')
+
+print('Average square footage is -- ' + str(avgSquareFootage / countSquareFootage))
+print('Average price is -- $' + str(avgPrice / countPrice))
