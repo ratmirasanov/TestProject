1a1ae020ad6e8bf85aa2c76605238c95e67e3de5
ratmirasanov <ratmir.asanov.7@gmail.com>
2017-11-08 21:15:54 +0200

    Used pylint.

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
-#Reading CSV-file to list of lists.
+"""Some test project for working with CSV-files."""
+
 import csv
 import sys
-exampleFile = open('Badprice.csv', 'rU')
-exampleReader = csv.reader(exampleFile)
-exampleData = list(exampleReader)
 
 
-#Declaration of variables.
-maxValue = 0
-minValue = sys.float_info.max
-maxIndex = ''
-minIndex = ''
-avgPrice = 0
-avgSquareFootage = 0
-countPrice = 0
-countSquareFootage = 0
+# Reading CSV-file to list of lists.
+EXAMPLE_FILE = open("Badprice.csv", "rU")
+EXAMPLE_READER = csv.reader(EXAMPLE_FILE)
+EXAMPLE_DATA = list(EXAMPLE_READER)
 
+# Declaration of variables.
+MAX_VALUE = 0
+MIN_VALUE = sys.float_info.max
+MAX_INDEX = ""
+MIN_INDEX = ""
+AVG_PRICE = 0
+AVG_SQUARE_FOOTAGE = 0
+COUNT_PRICE = 0
+COUNT_SQUARE_FOOTAGE = 0
 
-#Creation of two lists.
-for row in exampleData:
+# Creation of two lists.
+for row in EXAMPLE_DATA:
     if row[9].isalpha():
         continue
     else:
         if float(row[9]) > 0 and float(row[6]) > 0:
-            avgPrice += float(row[9])
-            avgSquareFootage += float(row[6])
-            countPrice += 1
-            countSquareFootage += 1
-            if (maxValue < float(row[9])):
-                 maxValue = float(row[9])
-                 maxIndex = row
-            if (minValue > float(row[9])):
-                 minValue = float(row[9])
-                 minIndex = row
+            AVG_PRICE += float(row[9])
+            AVG_SQUARE_FOOTAGE += float(row[6])
+            COUNT_PRICE += 1
+            COUNT_SQUARE_FOOTAGE += 1
+            if MAX_VALUE < float(row[9]):
+                MAX_VALUE = float(row[9])
+                MAX_INDEX = row
+            if MIN_VALUE > float(row[9]):
+                MIN_VALUE = float(row[9])
+                MIN_INDEX = row
 
-
-#Printing results.
-print('Max price of real estate -- ' + str(maxIndex) + ' is: ' + str(maxValue) + '$.')
-print('Min price of real estate -- ' + str(minIndex) + ' is: ' + str(minValue) + '$.')
-
-print('Average square footage is -- ' + str(avgSquareFootage / countSquareFootage))
-print('Average price is -- $' + str(avgPrice / countPrice))
+# Printing results.
+print("Max price of real estate -- " + str(MAX_INDEX) + " is: " + str(MAX_VALUE) + "$.")
+print("Min price of real estate -- " + str(MIN_INDEX) + " is: " + str(MIN_VALUE) + "$.")
+print("Average square footage is -- " + str(AVG_SQUARE_FOOTAGE / COUNT_SQUARE_FOOTAGE))
+print("Average price is -- $" + str(AVG_PRICE / COUNT_PRICE))
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 for i in {1..1000}
 do
